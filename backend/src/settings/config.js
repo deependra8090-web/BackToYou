fa1a18a54a20cdd3a0585de93d6f8b8d7f6a948b
Deependra Yadav <deependra9433@gmail.com>
2026-08-08 11:51:49 +0530

    commit

--- backend/src/settings/config.js
+++ backend/src/settings/config.js
@@ -40,6 +40,7 @@ const configuredOrigins = (process.env.ALLOWED_ORIGINS || "")
 
 const allowedOrigins = [
   ...configuredOrigins,
+  'https://back-to-you-xe9m.vercel.app',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000',
